232105908ada422dd599d9d8c57c3d80be0ec56a
Matt Kashani-Carver <matt@edflabs.net>
2026-01-10 10:36:01 +0000

    Updates

--- infra/bootstrap/main.tf
+++ infra/bootstrap/main.tf
@@ -42,20 +42,3 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "logicbot-terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name        = "LogicBot Terraform Locks"
-    Environment = "production"
-    ManagedBy   = "Terraform"
-  }
-}
